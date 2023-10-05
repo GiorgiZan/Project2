0bd36e8e38b87a8128e3fef56456816b14a7fd10
GiorgiZan <giorgizandarashvili7@gmail.com>
2023-10-05 18:04:12 +0400

    Final Project 2 DONE

--- src/main/java/DB/SQLQuery.java
+++ src/main/java/DB/SQLQuery.java
@@ -1,32 +1,10 @@
 package DB;
 
-import org.testng.annotations.Test;
 
 import java.sql.*;
 
 public class SQLQuery {
-//    public static void insertUser(String firstName, String lastName, String phone, String email, java.sql.Date dateOfBirth, String password) {
-//        try (Connection connection = DbConnection.getConnection();
-//             PreparedStatement preparedStatement = connection.prepareStatement(
-//                     "INSERT INTO users (firstName, lastName, phone, email, dateOfBirth, password) VALUES (?, ?, ?, ?, ?, ?)")) {
-//
-//            preparedStatement.setString(1, firstName);
-//            preparedStatement.setString(2, lastName);
-//            preparedStatement.setString(3, phone);
-//            preparedStatement.setString(4, email);
-//            preparedStatement.setDate(5, dateOfBirth);
-//            preparedStatement.setString(6, password);
-//
-//            int rowsAffected = preparedStatement.executeUpdate();
-//            if (rowsAffected != 1) {
-//                throw new RuntimeException("Insert operation failed");
-//            }
-//        } catch (SQLException e) {
-//            throw new RuntimeException("Error while inserting user data", e);
-//        }
-//    }
 
-    //java.sql.Date
     public static int insertUser(String firstName, String lastName, String phone, String email, java.sql.Date dateOfBirth, String password) {
         int generatedUserId = -1;
 
